3eb5ec219e41eb95e6b77840e58ccd7d9617e619
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-25 12:46:23 -0700

    Correct HTML syntax to simplify UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,28 @@ Pizza.prototype.purchaseSpecialPizza = function(size) {
 
 // UI logic
 
-const itsAPizza = new Pizza(["cheese", "veggie", "pepperoni"], ["margherita", "meatlover"], ["small", "medium", "large"]);
+
+function handlePizzaFormSubmission(event) {
+  event.preventDefault();
+  const selectClassicPizza = document.querySelector("select#classic-pizza").value;
+  const selectSpecialPizza = document.querySelector("select#special-pizza").value;
+  const selectSize = document.querySelector("select#size").value;
+  
+  if (selectClassicPizza || selectSpecialPizza){
+    const itsAPizza = new Pizza(selectClassicPizza, selectSpecialPizza, selectSize);
+    
+    if (selectClassicPizza) {
+      console.log(itsAPizza.purchaseClassicPizza(selectSize));
+    
+    } else if (selectSpecialPizza) {
+      console.log(itsAPizza.purchaseSpecialPizza(selectSize));
+    }
+  }
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#pizza-form").addEventListener("submit", handlePizzaFormSubmission);
+});
+
 
 
